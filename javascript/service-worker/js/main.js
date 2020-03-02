a884897125abe2a2ba3248acfdcf78bd8688019a
Pavel del Pozo <yosoy@paveldelpozo.es>
2020-03-02 18:27:37 +0100

    Add Vue Basic Example

--- javascript/service-worker/js/main.js
+++ javascript/service-worker/js/main.js
@@ -1,6 +1,59 @@
+function notify(title, body, shouldRequireInteraction = false, data = null) {
+    let random = Math.round(Math.random() * 1000);
+    let notification = new Notification(title, {
+        body : body,
+        icon: '/assets/pwa.png',
+        requireInteraction: shouldRequireInteraction,
+        data : data,
+        tag: `notification_${random}`
+    });
+    notification.addEventListener('click', event => {
+        let data = event.target.data;
+        console.log(data);
+    });
+    return notification;
+}
+
+function tryToNotify() {
+    let notification = null;
+    if (!window.Notification) {
+        alert("Este navegador no soporta las notificaciones del sistema");
+    } else if (Notification.permission === "granted") {
+        notification = notify(
+            "Hello",
+            `I'm a Notification with granted permission!`,
+            true,
+            {
+                url : window.location.href
+            }
+        );
+    } else if (Notification.permission !== 'denied') {
+        Notification.requestPermission()
+            .then(response => {
+                if (response === 'granted'){
+                    notification = notify(
+                        "Hello!",
+                        `I'm a Notification with granted permission!`,
+                        true,
+                        {
+                            url : window.location.href
+                        }
+                    );
+                }
+            });
+    }
+}
+
 if (navigator.serviceWorker) {
     console.log(`I can use Service Worker!`);
     navigator.serviceWorker.register('/sw.js');
 } else {
     console.log(`I can't use Service Worker, please update me...`);
 }
+
+let button = document.querySelector('.btn-success');
+button.addEventListener('click', event => {
+    event.preventDefault();
+    event.stopPropagation();
+    tryToNotify();
+});
--- javascript/service-worker/sw.js
+++ javascript/service-worker/sw.js
@@ -1,2 +1,50 @@
-console.log('SW: Hi!');
-// console.log('SW: Me descomentaron');
+self.addEventListener('install', event => {
+    // Download assets and resources
+    // Create local cache
+    // Create local database
+    console.log('SW: Installing...');
+
+    const installation = new Promise((resolve => {
+        setTimeout(() => {
+            console.log('SW: Installation complete!');
+            self.skipWaiting();
+            resolve();
+        }, 500);
+    }));
+
+    event.waitUntil(installation);
+});
+
+self.addEventListener('activate', event => {
+    // Remove old cache
+    console.log('SW: Activated and working...');
+});
+
+self.addEventListener('fetch', event => {
+    // Apply cache strategies
+});
+
+self.addEventListener('push', event => {
+    let random = Math.round(Math.random() * 1000);
+    self.registration.showNotification("Hello!", {
+        body: `I'm a Notification from SW`,
+        icon: '/assets/pwa.png',
+        tag: `Notification_${random}`,
+        requireInteraction: true,
+        data : {
+            url : 'http://localhost:8080'
+        }
+    })
+        .then(notificationEvent => {
+            console.log("SW Notification Event Promise");
+        });
+    //new Notification("Hello, I'm a Notification!");
+    // console.log('SW: Push notification received', event);
+});
+
+self.addEventListener('notificationclick', event => {
+    console.log(event);
+    console.log(event.notification.data);
+    event.notification.close();
+    clients.openWindow(event.notification.data.url);
+});
